98d6c518441b5bc368f2ce30815277bdd620ff0b
shebeerki <bismi71188@gmail.com>
2012-07-23 15:31:49 +0530

    circuit simiulation modified

--- Projects/circuitsimul.py
+++ Projects/circuitsimul.py
@@ -48,11 +48,19 @@ class Xor(Logic):
 		self.change = change
 	
 	def oper(self):
-		n1 = Not("not1", self.b)
-		a1 = And("and1", self.a, n1.nprints())
+		w1 = Wire('wire1', self.b, "not1")
+		w2 = Wire("wire2", self.a, "and1")
+		n1 = Not("not1", w1.woper())
+		w3 = Wire("wire3", n1.nprints(), "and1")
+		a1 = And("and1", w1.woper(), n1.noper())
+		w4 = Wire("wire4", self.b, "and2")
+		w5 = Wire('wire5', self.a, "not2")
 		n2 = Not("not2", self.a)
-		a2 = And("and2", n2.nprints(), self.b)
-		o  = Or("xor1", a1.aprints(), a2.aprints())
+		w6 = Wire("wire6", n2.nprints(), "and2")
+		a2 = And("and2", n2.noper(), w2.woper())
+		w7 = Wire("wire7", a1.aprints(), "or")
+		w8 = Wire("wire8", a2.aprints(), "or")
+		o  = Or("xor1", a1.aoper(), a2.aoper())
 		o.oprints()
 		return 0
 
@@ -73,6 +81,19 @@ class Xor(Logic):
 			if self.working():
 				self.oper()
 		return 0
+
+class Wire(And, Not, Or):
+	
+		def __init__(self, name, a_now, gate = None):
+			self.name = name
+			self.a_now = a_now
+			self.gate = gate
+			print "%s passes    %d ==>   %s\n"%(self.name, self.a_now, self.gate)
+
+		def woper(self):
+			return self.a_now
+
+
 (a, b) = input("enter the inputs a and b where\
 	a, b are either binary 1 or 0\n")
 c = Xor('xor1', a, b, 0)
